9cabd9ba76d9849192ffc975c2447bf7af349343
undancer <undancer@gmail.com>
2018-09-12 21:23:07 +0800

    添加两个带UI的按钮

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Filter from "./Filter";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Footer = (props) => {
     const {count, filter, changeFilter} = props;
     return (
         <div className="todo-footer clearfix">
             <div className="float-left">
-                <strong>
-                    <span className="count-todos">{count}</span>
-                </strong>
-                {' items left'}
+                <div className="pull-left buttons">
+                    <a className="button add"> <FontAwesomeIcon icon="plus"/></a>
+                    <a className="button search"><FontAwesomeIcon icon="search"/></a>
+                </div>
+                {`${count} items left`}
             </div>
             <div className="float-right">
                 <Filter filter={filter} change={changeFilter}/>
