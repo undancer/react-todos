1cc473b6f5dd9afce14b27e96db8d366206141a2
undancer <undancer@gmail.com>
2018-09-12 17:36:21 +0800

    使用数组数据来渲染页面

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import React from 'react';
 
 const App = () => {
+    let items = [
+        {
+            text: 'Learn Javascript',
+            completed: false
+        },
+        {
+            text: 'Learn React',
+            completed: false
+        },
+        {
+            text: 'Build a React App',
+            completed: true
+        }
+    ];
     return (
         <div className="container">
             <div className="row">
                 <div className="todolist">
                     <h1>Todos</h1>
                     <ul className="list-unstyled">
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Take out the trash</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Buy bread</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Teach penguins to fly</label>
-                            </div>
-                        </li>
+                        {
+                            items.map((item) => (
+                                    <li className="ui-state-default">
+                                        <div className="checkbox">
+                                            <label>
+                                                <input type="checkbox" value="" checked={item.completed}/>{item.text}
+                                            </label>
+                                        </div>
+                                    </li>
+                                )
+                            )
+                        }
                     </ul>
                 </div>
             </div>
