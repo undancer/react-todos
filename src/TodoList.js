69fc9d07dd73d5e26e936814cab074f29cc1bb3f
undancer <undancer@gmail.com>
2018-09-12 19:20:52 +0800

    将TodoList转化为有状态的组件

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,34 +4,36 @@ import Header from "./Header";
 import Footer from "./Footer";
 import {applyFilter} from "./filter/fitler";
 
-const TodoList = (props) => {
-    const {title, items, addNew, changeFilter, filter} = props;
-    const count = items.length;
-    const filteredList = applyFilter(items, filter);
-    return (
-        <div className="todolist">
-            <Header
-                title={title}
-                addNew={addNew}
-            />
-            {
-                filteredList.length > 0 ?
-                    (
-                        <ul className="list-unstyled">
-                            {
-                                filteredList.map((item) => (
-                                    <TodoItem item={item}/>
-                                ))
-                            }
-                        </ul>
-                    ) : (
-                        <p className="alert alert-info">There are no items.</p>
-                    )
-            }
+class TodoList extends React.Component {
+    render() {
+        const {title, items, addNew, changeFilter, filter} = props;
+        const count = items.length;
+        const filteredList = applyFilter(items, filter);
+        return (
+            <div className="todolist">
+                <Header
+                    title={title}
+                    addNew={addNew}
+                />
+                {
+                    filteredList.length > 0 ?
+                        (
+                            <ul className="list-unstyled">
+                                {
+                                    filteredList.map((item) => (
+                                        <TodoItem item={item}/>
+                                    ))
+                                }
+                            </ul>
+                        ) : (
+                            <p className="alert alert-info">There are no items.</p>
+                        )
+                }
 
-            <Footer count={count} filter={filter} changeFilter={changeFilter}/>
-        </div>
-    );
-};
+                <Footer count={count} filter={filter} changeFilter={changeFilter}/>
+            </div>
+        );
+    }
+}
 
 export default TodoList;
