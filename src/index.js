e7cb2f04152901a5db3cecb5e58e7dc40d1741ec
undancer <undancer@gmail.com>
2018-09-12 17:19:30 +0800

    使用最新的bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
 
 ReactDOM.render(
     (
