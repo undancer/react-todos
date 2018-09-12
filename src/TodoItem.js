75ed50eb038ae79f7abd7ee1a713e353b5b15ad4
undancer <undancer@gmail.com>
2018-09-12 20:18:47 +0800

    任务完成时更新样式

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,8 +4,9 @@ import CheckBox from "./CheckBox";
 const TodoItem = (props) => {
     const {id, data, completed, changeStatus} = props;
     const handleChange = (checked) => changeStatus(id, checked);
+    const className = 'todo-item ui-state-default ' + (completed === true ? 'completed' : 'pending');
     return (
-        <li key={id} className="ui-state-default">
+        <li key={id} className={className}>
             <div className="checkbox">
                 <label>
                     <CheckBox checked={completed} onChange={handleChange}/>{data}
