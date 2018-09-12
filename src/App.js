03dd297578b276abd9a9906f8a45257be40e192c
undancer <undancer@gmail.com>
2018-09-12 18:09:05 +0800

    将App转化成有状态的组件

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import TodoList from "./TodoList";
 
-const App = () => {
-    let title = 'Things to do';
-    let items = [
-        {
-            id: 1,
-            text: 'Learn Javascript',
-            completed: false
-        },
-        {
-            id: 2,
-            text: 'Learn React',
-            completed: false
-        },
-        {
-            id: 3,
-            text: 'Build a React App',
-            completed: true
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    text: 'Learn Javascript',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    text: 'Learn React',
+                    completed: false
+                },
+                {
+                    id: 3,
+                    text: 'Build a React App',
+                    completed: true
+                }
+            ]
         }
-    ];
-    return (
-        <div className="container">
-            <div className="row">
-                <TodoList title={title} items={items}/>
+    }
+
+    render() {
+        let title = 'Things to do';
+        return (
+            <div className="container">
+                <div className="row">
+                    <TodoList title={title} items={this.state.items}/>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
