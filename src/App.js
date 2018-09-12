380ca77576c9ef5febc5f7ea39a47113a669bbbe
undancer <undancer@gmail.com>
2018-09-12 17:25:34 +0800

    修改jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from 'react';
 
-const App = () => (<h1 className="text-center">Hello World</h1>);
+const App = () => (
+    <div className="container">
+        <div className="row">
+            <div className="todolist">
+                <h1>Todos</h1>
+                <ul className="list-unstyled">
+                    <li className="ui-state-default">
+                        <div className="checkbox">
+                            <label>
+                                <input type="checkbox" value=""/>Take out the trash</label>
+                        </div>
+                    </li>
+                    <li className="ui-state-default">
+                        <div className="checkbox">
+                            <label>
+                                <input type="checkbox" value=""/>Buy bread</label>
+                        </div>
+                    </li>
+                    <li className="ui-state-default">
+                        <div className="checkbox">
+                            <label>
+                                <input type="checkbox" value=""/>Teach penguins to fly</label>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+);
 
 export default App;
