8ed921f6255a02508d9c7aa5af0bd3e3cff239d0
undancer <undancer@gmail.com>
2018-09-18 21:30:28 +0800

    重构少数文件

---
+++ src/config/lang/en_US.ts
@@ -0,0 +1,5 @@
+const MSG_NO_ITEMS = 'There are no items.';
+const INFO_SHORTCUT_KEYS = 'Press `/` to search and `N` to create a new item.';
+const INFO_CANCEL_SHORTCUT_KEY = 'Press `Esc` to cancel.';
+
+export {MSG_NO_ITEMS, INFO_SHORTCUT_KEYS, INFO_CANCEL_SHORTCUT_KEY}
