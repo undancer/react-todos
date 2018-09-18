42443c10a4c4093a2a6bde1ff5cb788099037118
undancer <undancer@gmail.com>
2018-09-18 21:10:43 +0800

    先让ts运行起来

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './components/wrappers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css'
