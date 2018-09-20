561a4898f59cb78d1931a37b813222f7d3922c29
undancer <undancer@gmail.com>
2018-09-20 10:15:39 +0800

    使用router来切换

--- src/components/todomvc/Filter.tsx
+++ src/components/todomvc/Filter.tsx
@@ -1,21 +1,43 @@
 import * as React from "react";
+import {Dispatch} from "redux";
+import {connect} from "react-redux";
+import {NavLink, withRouter} from "react-router-dom";
+
+const mapStateToProps = (state: any) => ({});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
 
 class Filter extends React.Component {
     render() {
         return (
             <ul className="filters">
                 <li>
-                    <a className="selected" href="#/">All</a>
+                    <NavLink
+                        exact
+                        activeClassName="selected"
+                        to="/"
+                    >
+                        All
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="#/active">Active</a>
+                    <NavLink
+                        activeClassName="selected"
+                        to="/active"
+                    >
+                        Active
+                    </NavLink>
                 </li>
                 <li>
-                    <a href="#/completed">Completed</a>
+                    <NavLink
+                        activeClassName="selected"
+                        to="/completed">
+                        Completed
+                    </NavLink>
                 </li>
             </ul>
         );
     }
 }
 
-export default Filter;
+export default withRouter<any>(connect(mapStateToProps, mapDispatchToProps)(Filter));
--- src/components/todomvc/Footer.tsx
+++ src/components/todomvc/Footer.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import Filter from "./Filter";
+import {Dispatch} from "redux";
+import {connect} from "react-redux";
+import {withRouter} from "react-router";
 
-class Footer extends React.Component {
+const mapStateToProps = (state: any) => ({
+    items: state.todos.items,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
+
+interface IFooterProps {
+    items: [],
+}
+
+class Footer extends React.Component<IFooterProps> {
     render() {
+        const {items} = this.props;
+        const count = items.length;
         return (
             <footer className="footer">
-                <span className="todo-count"><strong>0</strong> item left</span>
+                <span className="todo-count"><strong>{count}</strong> item left</span>
                 <Filter/>
                 <button className="clear-completed">Clear completed</button>
             </footer>
@@ -13,4 +28,4 @@ class Footer extends React.Component {
     }
 }
 
-export default Footer;
+export default withRouter<any>(connect(mapStateToProps, mapDispatchToProps)(Footer));
