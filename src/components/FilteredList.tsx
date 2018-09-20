f82e3880a51770da228d5dbaf232e84d78ef61a9
undancer <undancer@gmail.com>
2018-09-20 12:20:42 +0800

    清理文件

--- src/components/FilteredList.tsx
+++ src/components/FilteredList.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {connect} from "react-redux";
-import {applyFilter} from "../../utils/fitler";
+import {applyFilter} from "../utils/fitler";
 import TodoList from "./TodoList";
 
 const mapStateToProps = (state: { todos: { items: [] } }) => ({
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/wrappers/App';
 import registerServiceWorker from './registerServiceWorker';
 // import './index.css'
 // import './config/bootstrap'
@@ -9,18 +8,13 @@ import 'todomvc-app-css/index.css'
 import {Provider} from "react-redux";
 import store from "./stores";
 import {BrowserRouter as Router} from "react-router-dom";
-import TodoApp from "./components/todomvc/TodoApp";
+import TodoApp from "./components/TodoApp";
 
 ReactDOM.render(
     (
         <Provider store={store}>
             <Router>
-                <div>
-                    <div>
-                        <TodoApp/>
-                    </div>
-                    <App/>
-                </div>
+                <TodoApp/>
             </Router>
         </Provider>
     ), document.getElementById('root'));
